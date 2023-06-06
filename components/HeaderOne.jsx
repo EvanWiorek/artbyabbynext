29bd6a5ccf42da27e975d092215033b443f86cf0
Evan Wiorek <124269000+EvanWiorek@users.noreply.github.com>
2023-06-06 11:18:49 -0500

    added video

--- components/HeaderOne.jsx
+++ components/HeaderOne.jsx
@@ -1,11 +1,15 @@
 const HeaderOne = () => {
   return (
-    <div className="parallax-container" >
-      <div className="parallax">
-        {/* <img src="/static/images/header-bg.jpg" alt="Header One" /> */}
-        <img src="/static/videos/header-one-4.gif" alt="Header One" style={{ height: `auto` }} />
-        {/* <video src="/static/videos/header-one.mp4" autoplay muted loop></video> */}
-      </div>
+    // <div className="parallax-container" >
+    //   <div className="parallax">
+    //     {/* <img src="/static/images/header-bg.jpg" alt="Header One" /> */}
+    //     {/* <img src="/static/videos/header-one-4.gif" alt="Header One" style={{ height: `auto` }} /> */}
+        
+    //   </div>
+    // </div>
+    <div className="parallax-container">
+      
+      <video src="/static/videos/header-one.mp4" autoPlay muted loop controls></video>
     </div>
   )
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,10 @@
 import Head from 'next/head'
 import Layout from '@/components/Layout'
 import { useState, useRef, useMemo, useEffect } from 'react';
-import $ from "jquery";
 import HeaderOne from '../components/HeaderOne'
 
 
 export default function Home() {
-
-  useEffect(() => {
-    let elems
-
-    if (typeof window !== 'undefined') {
-      const M = require('materialize-css');
-      elems = document.querySelectorAll('.parallax');
-      M.Parallax.init(elems);
-    }
-
-  }, [])
 
   return (
     <Layout home>
