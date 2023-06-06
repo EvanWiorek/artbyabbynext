f2b769164958b77af153fb626b9a11144b1bdf86
Evan Wiorek <124269000+EvanWiorek@users.noreply.github.com>
2023-06-06 09:51:58 -0500

    update

--- components/HeaderOne.jsx
+++ components/HeaderOne.jsx
@@ -2,8 +2,8 @@ const HeaderOne = () => {
   return (
     <div className="parallax-container" >
       <div className="parallax">
-        <img src="/static/images/header-bg.jpg" alt="Header One" style={{ height: `auto` }} />
-        {/* <img src="/static/videos/header-one-4.gif" alt="Header One" style={{ height: `auto` }} /> */}
+        {/* <img src="/static/images/header-bg.jpg" alt="Header One" /> */}
+        <img src="/static/videos/header-one-4.gif" alt="Header One" style={{ height: `auto` }} />
         {/* <video src="/static/videos/header-one.mp4" autoplay muted loop></video> */}
       </div>
     </div>
