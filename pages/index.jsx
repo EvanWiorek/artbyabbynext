076fd8abb52805c66038b17bb5669988cf41207b
Evan Wiorek <124269000+EvanWiorek@users.noreply.github.com>
2023-06-06 07:08:48 -0500

    with spring parallax

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,52 +1,26 @@
 import Head from 'next/head'
 import Layout from '@/components/layout'
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import { useInView } from '@react-spring/web'
 import { useState, useRef, useMemo, useEffect } from 'react'
 
 export default function Home() {
-  const targetRef = useRef(null)
-  const [isVisible, setIsVisible] = useState(false);
-
-  const callbackFunction = entries => {
-    const [entry] = entries;
-    setIsVisible(entry.isIntersecting);
-  }
-
-  const options = useMemo(() => {
-    return {
-      root: null,
-      rootMargin: `0px`,
-      threshold: 0.3
-    }
-  }, []);
+  // const myRef = useRef(null);
+  const [ref, inView] = useInView();
 
   useEffect(() => {
-    const observer = new IntersectionObserver(callbackFunction, options);
-    const currentTarget = targetRef.current;
-    if(currentTarget) {
-      observer.observe(currentTarget); 
-    };
-
-    if(isVisible) {
-      document.querySelector(".logo-img").style = "transform: scale(.7);"
-    }
-    else if(!isVisible) {
-      document.querySelector(".logo-img").style = "transform: scale(1);"
-    }
-
-    return () => {
-      if(currentTarget) {
-        observer.unobserve(currentTarget); 
-      }
-    }
-  }, [targetRef, options])
-
+    // const observer = new IntersectionObserver((entries) => {
+    //   const entry = entries[0];
+    //   console.log('entry', entry);
+    // })
+    // return observer.observe(myRef.current);
+  }, [])
+  
   return (
     <Layout home>
       <Head>
         <title>Art By Abby | Home</title>
       </Head>
-      
       <main>
         <Parallax pages={3}>
           <ParallaxLayer offset={0} speed={.4} factor={2}>
@@ -54,7 +28,7 @@ export default function Home() {
           </ParallaxLayer>
 
           <ParallaxLayer offset={.5}
-          speed={0.05} className='text-center'>
+            speed={0.05} className='text-center'>
             <h1 className='display-4'>Welcome to Art By Abby</h1>
           </ParallaxLayer>
 
@@ -62,7 +36,7 @@ export default function Home() {
           </ParallaxLayer>
 
           <ParallaxLayer offset={.9}
-          speed={0.05}>
+            speed={0.05}>
             <div className="body-white">
               <br />
               <br />
@@ -87,7 +61,10 @@ export default function Home() {
               <br />
               <br />
               <br />
-              <h1 ref={targetRef}>TEST</h1>
+
+              <div ref={ref}>
+                <h1>{inView ? 'Hello World' : 'TEST'}</h1>
+              </div>
             </div>
           </ParallaxLayer>
 
