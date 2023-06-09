05cafc23c67f9666a2c0b6ca04f1b29c884da651
Evan Wiorek <124269000+EvanWiorek@users.noreply.github.com>
2023-06-08 23:06:37 -0500

    admin page wip

--- pages/adminpage.jsx
+++ pages/adminpage.jsx
@@ -58,8 +58,8 @@ const AdminPage = ({ isVisible }) => {
         </div>
 
         {/* background animation */}
-        <div class="area" >
-          <ul class="circles">
+        <div className="area" >
+          <ul className="circles">
             <li></li>
             <li></li>
             <li></li>
